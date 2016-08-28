682312e8bc14c6cd5759004eea1c395ab402d7d7
Brian Martinez <marjosep@msudenver.edu>
2016-08-27 20:55:20 -0600

    Initial version of fizzbuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    class Program
+    class Counter
     {
         static void Main(string[] args)
         {
+            string fb = "";
+
+            Console.WriteLine("FizzBuzz numbers:");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fb = (i % 3 == 0) ? "Fizz" : "";
+                fb += (i % 5 == 0) ? "Buzz" : "";
+                fb += ((i % 3 > 0) && (i % 5 > 0)) ? i.ToString() : "";
+                Console.WriteLine(fb);
+            }
+
+            Console.Write("Press a key to exit...");
+            Console.ReadKey();
         }
     }
 }
